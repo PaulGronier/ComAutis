43549ae36dcdedb93cf4e6fba40bdf251f317714
Quibron <paul.gronier@gmail.com>
2018-01-15 15:50:55 +0100

    First project commit

---
+++ app/src/main/java/com/example/paulg/comautis/mvp/view/View.java
@@ -0,0 +1,13 @@
+package com.example.paulg.comautis.mvp.view;
+
+/**
+ * Created by paulg on 15/01/2018.
+ */
+
+public interface View {
+    /**
+     * FIXME
+     * implements void error(String message);
+     * since it'a already everywhere
+     */
+}
