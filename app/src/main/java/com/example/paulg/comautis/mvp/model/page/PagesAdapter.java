11847056a7a5951456ed302bb26ffe38c9f943b0
Quibron <paul.gronier@gmail.com>
2018-01-18 15:50:35 +0100

    [ADD] ChoosePageActivity/Adapter

--- app/src/main/java/com/example/paulg/comautis/mvp/model/page/PagesAdapter.java
+++ app/src/main/java/com/example/paulg/comautis/mvp/model/page/PagesAdapter.java
@@ -1,16 +1,16 @@
 package com.example.paulg.comautis.mvp.model.page;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.paulg.comautis.R;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,42 +19,71 @@ import butterknife.ButterKnife;
  * Created by paulg on 17/01/2018.
  */
 
-public class PagesAdapter extends ArrayAdapter <Page> {
+public class PagesAdapter extends RecyclerView.Adapter<PagesAdapter.PagesHolder> {
+
+    private final Context mContext;
+    private final OnClickListener mListener;
+
+    private final List<Page> mPages;
 
 
-
-    public PagesAdapter(Context context, ArrayList<Page> pages) {
-        super(context, 0, pages);
+    public PagesAdapter(Context context, List<Page> pages, OnClickListener listener) {
+        mContext = context;
+        mListener = listener;
+        mPages = pages;
     }
 
     @Override
-    public View getView (int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+    public PagesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(mContext).inflate(R.layout.item_page, parent, false);
 
-        Page page = getItem(position);
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_page, parent, false);
-            holder = new ViewHolder(convertView);
-            convertView.setTag(holder);
-        } else { holder = (ViewHolder) convertView.getTag(); }
-
-        holder.mPagePictureView.setImageResource(R.drawable.ic_page);
-        holder.mPageTitleView.setText(page.getTitle());
-        holder.mModifButtonView.setImageResource(R.drawable.modif);
-        holder.mRemoveButtonView.setImageResource(R.drawable.ic_action_name);
-
-        return convertView;
+        return new PagesHolder(view);
     }
 
-    static class ViewHolder {
-        @BindView(R.id.page_pic) ImageView mPagePictureView;
-        @BindView(R.id.page_title) TextView mPageTitleView;
-        @BindView(R.id.modif_button) ImageView mModifButtonView;
-        @BindView(R.id.remove_button) ImageView mRemoveButtonView;
+    @Override
+    public void onBindViewHolder(PagesHolder holder, int position) {
+        holder.bindButtons(mPages.get(position));
+    }
 
-        public ViewHolder(View view) {
-            ButterKnife.bind(this, view);
+    @Override
+    public int getItemCount() {
+        return mPages.size();
+    }
+
+    public interface OnClickListener {
+        void onModifClickItem();
+
+        void onRemoveClickItem();
+    }
+
+    public class PagesHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.page_pic)
+        ImageView mPagePictureView;
+        @BindView(R.id.page_title)
+        TextView mPageTitleView;
+        @BindView(R.id.modif_button)
+        ImageView mModifButtonView;
+        @BindView(R.id.remove_button)
+        ImageView mRemoveButtonView;
+
+        View mView;
+
+        public PagesHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindButtons(Page page) {
+            mPagePictureView.setImageResource(R.drawable.ic_page);
+            mPageTitleView.setText(page.getTitle());
+            mModifButtonView.setImageResource(R.drawable.modif);
+            mRemoveButtonView.setImageResource(R.drawable.ic_action_name);
+
+            mModifButtonView.setOnClickListener(v -> mListener.onModifClickItem());
+            mRemoveButtonView.setOnClickListener(v -> mListener.onRemoveClickItem());
         }
     }
 }
--- app/src/main/java/com/example/paulg/comautis/ui/page/ChoosePageActivity.java
+++ app/src/main/java/com/example/paulg/comautis/ui/page/ChoosePageActivity.java
@@ -2,8 +2,10 @@ package com.example.paulg.comautis.ui.page;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.paulg.comautis.R;
 import com.example.paulg.comautis.mvp.model.page.Page;
@@ -14,36 +16,45 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by paulg on 16/01/2018.
  */
 
-public class ChoosePageActivity extends AppCompatActivity {
+public class ChoosePageActivity extends AppCompatActivity implements PagesAdapter.OnClickListener {
 
-    @BindView(R.id.lv_pages) ListView mListPagesView;
+    @BindView(R.id.lv_pages)
+    RecyclerView mListPagesView;
 
-    private List<Page> mListPages = new ArrayList<>();
+    private List<Page> mListPages;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_list_pages);
         ButterKnife.bind(this);
         init();
-
-
     }
 
+    @Override
+    public void onModifClickItem() {
+        Toast.makeText(this, "Modification", Toast.LENGTH_SHORT).show();
+    }
 
-    private void init () {
-        ArrayList<Page> pages = new ArrayList<>();
-        pages.add(new Page("Sortie à la plage"));
-        PagesAdapter mPagesAdapter = new PagesAdapter(this, pages);
+    @Override
+    public void onRemoveClickItem() {
+        Toast.makeText(this, "Suppression", Toast.LENGTH_SHORT).show();
+    }
 
+    private void init() {
+        mListPages = new ArrayList<>();
+        mListPages.add(new Page("Sortie à la plage"));
+
+        PagesAdapter mPagesAdapter = new PagesAdapter(this, mListPages, this);
         mListPagesView.setAdapter(mPagesAdapter);
+        mListPagesView.setLayoutManager(new LinearLayoutManager(this));
+        mListPagesView.setVisibility(View.VISIBLE);
     }
 
 
